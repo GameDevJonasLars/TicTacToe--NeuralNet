2cbe1ecbee7915ef261478f0d9edf4d5d62df41b
JoJotoPlay <JoJotoPlay@users.noreply.github.com>
2018-05-17 21:59:16 +0200

    added pack SomeFunktions

---
+++ TicTacToeNeuralNet/src/com/SomeFunktions/ArrayListFunktions.java
@@ -0,0 +1,30 @@
+package com.SomeFunktions;
+
+import java.util.ArrayList;
+
+public class ArrayListFunktions {
+
+	public static int maxI (ArrayList<Integer> iList) {
+		int max = 0;
+		
+		for (int i = 0; i < iList.size(); i++) {
+			if (iList.get(i) > max) {
+				max = iList.get(i);
+			}
+		}
+		return max;	
+	}
+	
+	public static double maxD (ArrayList<Double> iList) {
+		double max = 0.0;
+		
+		for (int i = 0; i < iList.size(); i++) {
+			if (iList.get(i) > max) {
+				max = iList.get(i);
+			}
+		}
+		return max;	
+	}
+	
+
+}
